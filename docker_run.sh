2ef13b6e49ffc14ef3f2488f4caa428509f5b807
Ivan Poon <ravenxce@gmail.com>
2018-12-04 16:58:32 +0800

    Update neo-cli to 2.9.2

--- docker_run.sh
+++ docker_run.sh
@@ -3,6 +3,7 @@
 # Start a Docker container which runs the four consensus nodes. If it is
 # already running, it will be destroyed first.
 #
+TAG_NAME="2.9.2"
 CONTAINER_NAME="neo-privnet"
 CONTAINER=$(docker ps -aqf name=$CONTAINER_NAME)
 
@@ -14,4 +15,17 @@ if [ -n "$CONTAINER" ]; then
 fi
 
 echo "Starting container..."
-docker run -d --name $CONTAINER_NAME -p 20333-20336:20333-20336/tcp -p 30333-30336:30333-30336/tcp -h $CONTAINER_NAME $CONTAINER_NAME
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p 20333-20336:20333-20336/tcp \
+  -p 30333-30336:30333-30336/tcp \
+  -v neo-2_9_2-blockchain-1:/opt/node1/neo-cli/Chain \
+  -v neo-2_9_2-blockchain-2:/opt/node2/neo-cli/Chain \
+  -v neo-2_9_2-blockchain-3:/opt/node3/neo-cli/Chain \
+  -v neo-2_9_2-blockchain-4:/opt/node4/neo-cli/Chain \
+  --mount type=bind,source=/opt/chain.acc,target=/opt/node1/neo-cli/chain.acc,readonly \
+  --mount type=bind,source=/opt/chain.acc,target=/opt/node2/neo-cli/chain.acc,readonly \
+  --mount type=bind,source=/opt/chain.acc,target=/opt/node3/neo-cli/chain.acc,readonly \
+  --mount type=bind,source=/opt/chain.acc,target=/opt/node4/neo-cli/chain.acc,readonly \
+  -h $CONTAINER_NAME \
+  $CONTAINER_NAME:$TAG_NAME
