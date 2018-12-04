5aacec1d717ebd1e8f6507ecdabba5d6b8ed1336
Ivan Poon <ravenxce@gmail.com>
2018-12-04 19:48:49 +0800

    Fix resync script

--- scripts/resync.sh
+++ scripts/resync.sh
@@ -2,9 +2,10 @@
 #
 # This script starts four consensus and waits forever
 #
-screen -dmS node1 dotnet /opt/node1/neo-cli/neo-cli.dll --rpc
-screen -dmS node2 dotnet /opt/node2/neo-cli/neo-cli.dll --rpc
-screen -dmS node3 dotnet /opt/node3/neo-cli/neo-cli.dll --rpc
-screen -dmS node4 dotnet /opt/node4/neo-cli/neo-cli.dll --rpc
+
+screen -dmS node1 expect /opt/start_node.sh /opt/node1/neo-cli/
+screen -dmS node2 expect /opt/start_node.sh /opt/node2/neo-cli/
+screen -dmS node3 expect /opt/start_node.sh /opt/node3/neo-cli/
+screen -dmS node4 expect /opt/start_node.sh /opt/node4/neo-cli/
 
 sleep infinity
